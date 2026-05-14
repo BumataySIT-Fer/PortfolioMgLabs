2fbdf7f3cbe69e22dd8ff5825bf7fc2a07124664
BumataySIT-Fer <20253253@s.ubaguio.edu>
2026-05-14 22:20:23 +0800

    waw

--- src/Links/Home.tsx
+++ src/Links/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import pro from '../assets/pro.png';
 
 const Home: React.FC = () => {
